874f3104db7bc31329fa297905f79f50c52bf445
[BIT-zhangxin] <1047632321@qq.com>
2019-08-26 17:08:19 +0800

    Initial commit

---
+++ src/main/java/com/example/labwebsystem/LabwebsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.labwebsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabwebsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabwebsystemApplication.class, args);
+    }
+
+}
