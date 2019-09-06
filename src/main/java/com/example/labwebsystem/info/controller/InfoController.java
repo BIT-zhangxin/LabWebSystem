0ce11e86255ba89816b68b4121a8c1d0d2a24127
孤舟睡客 <30551160+bt-1120161955@users.noreply.github.com>
2019-09-06 22:13:40 +0800

    提交inforController和inforMapper

--- src/main/java/com/example/labwebsystem/info/controller/InfoController.java
+++ src/main/java/com/example/labwebsystem/info/controller/InfoController.java
@@ -5,7 +5,9 @@ import com.example.labwebsystem.info.service.InfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.labwebsystem.entity.*;
 
+import java.util.List;
 
 @RestController
 @RequestMapping("/info")
@@ -17,4 +19,102 @@ public class InfoController {
     @Autowired
     InfoMapper infoMapper;
 
+    //对t_teacher表格操作
+    @RequestMapping("/selectTeacher")
+    public  List<Teacher> selectTeacher(int category){
+        return infoMapper.selectTeacher(category);
+    }
+
+    //对t_student表格进行操作
+    @RequestMapping("/selectGraduate")
+    public  List<Student> selectGraduate(){
+        return infoMapper.selectGraduate();
+    }
+
+    //对t_static_content表格进行操作
+    @RequestMapping("/selectStaticContent")
+    public StaticContent selectStaticContent(int staticContentId){
+        return infoMapper.selectStaticContent(staticContentId);
+    }
+
+    //对t_dynamic表格操作
+    @RequestMapping("/selectDynamicByType")
+    public List<Dynamic> selectDynamicByType(int type,int currentPage,int pageSize){
+        int offset = (currentPage -1 ) * pageSize;
+        return infoMapper.selectDynamicByType(type,offset,pageSize);
+    }
+    @RequestMapping("/selectDynamic")
+    public Dynamic selectDynamic(int dynamicId){
+        return infoMapper.selectDynamic(dynamicId);
+    }
+    @RequestMapping("/selectDynamicByCondition")
+    public List<Dynamic> selectDynamicByCondition(int type,String condition){
+        return infoMapper.selectDynamicByCondition(type,condition);
+    }
+
+    //对t_teaching_information表格操作
+    @RequestMapping("/selectTeachingInformation")
+    public List<TeachingInformation> selectTeachingInformation(int currentPage,int pageSize){
+        int offset = (currentPage -1 ) * pageSize;
+        return infoMapper.selectTeachingInformation(offset,pageSize);
+    }
+    @RequestMapping("/selectTeachingInformationByCondition")
+    public List<TeachingInformation> selectTeachingInformationByCondition(String condition){
+        return infoMapper.selectTeachingInformationByCondition(condition);
+    }
+
+    //对t_scientific_information表格操作
+    @RequestMapping("/selectScientificInformation")
+    public List<ScientificInformation> selectScientificInformation(int currentPage,int pageSize){
+        int offset = (currentPage -1 ) * pageSize;
+        return infoMapper.selectScientificInformation(offset,pageSize);
+    }
+    @RequestMapping("/selectScientificInformationByCondition")
+    public List<ScientificInformation> selectScientificInformationByCondition(String condition){
+        return infoMapper.selectScientificInformationByCondition(condition);
+    }
+
+    //对t_research_result表格操作
+    @RequestMapping("/selectResearchResult")
+    public List<ResearchResult> selectResearchResult(int currentPage,int pageSize){
+        int offset = (currentPage -1 ) * pageSize;
+        return infoMapper.selectResearchResult(offset,pageSize);
+    }
+    @RequestMapping("/selectResearchResultByCondition")
+    public List<ResearchResult> selectResearchResultByCondition(String condition){
+        return infoMapper.selectResearchResultByCondition(condition);
+    }
+
+    //对t_teach_result表格操作
+    @RequestMapping("/selectTeachResult")
+    public List<TeachResult> selectTeachResult(int currentPage,int pageSize){
+        int offset = (currentPage -1 ) * pageSize;
+        return infoMapper.selectTeachResult(offset,pageSize);
+    }
+    @RequestMapping("/selectTeachResultByCondition")
+    public List<TeachResult> selectTeachResultByCondition(String condition){
+        return infoMapper.selectTeachResultByCondition(condition);
+    }
+
+    //对t_research_paper表格操作
+    @RequestMapping("/selectResearchPaper")
+    public List<ResearchPaper> selectResearchPaper(int currentPage,int pageSize){
+        int offset = (currentPage -1 ) * pageSize;
+        return infoMapper.selectResearchPaper(offset,pageSize);
+    }
+    @RequestMapping("/selectResearchPaperByCondition")
+    public List<ResearchPaper> selectResearchPaperByCondition(String condition){
+        return infoMapper.selectResearchPaperByCondition(condition);
+    }
+
+    //对t_teach_paper表格操作
+    @RequestMapping("/selectTeachPaper")
+    public List<TeachPaper> selectTeachPaper(int currentPage,int pageSize){
+        int offset = (currentPage -1 ) * pageSize;
+        return infoMapper.selectTeachPaper(offset,pageSize);
+    }
+    @RequestMapping("/selectTeachPaperByCondition")
+    public List<TeachPaper> selectTeachPaperByCondition(String condition){
+        return infoMapper.selectTeachPaperByCondition(condition);
+    }
 }
--- src/main/java/com/example/labwebsystem/info/mapper/InfoMapper.java
+++ src/main/java/com/example/labwebsystem/info/mapper/InfoMapper.java
@@ -1,11 +1,99 @@
 package com.example.labwebsystem.info.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.core.annotation.Order;
+import com.example.labwebsystem.entity.*;
+
+import java.util.Date;
+import java.util.List;
 
 @Mapper
 @Order(1)
 public interface InfoMapper {
+
+    //对t_teacher表格操作
+    @Select("SELECT id,job_number as jobNumber,name,last_name as lastName,first_name as firstName," +
+            "sex,category,nationality,unit,job_title as jobTitle ,tutor_qualification as tutorQualification" +
+            "position,laboratory_position as laboratoryPosition,birthday,email,mobile_phone as mobilePhone,office_phone as officePhone ,remarks,photo" +
+            "from t_teacher WHERE category` = #{param1}")
+    List<Teacher> selectTeacher(int category);
+
+    //对t_student表格进行操作
+    @Select("SELECT id,student_number as studentNumber,name,last_name as lastName ,first_name as firstName," +
+            "sex,category,nationality,admission_time as admissionTime,graduation_time as graduationTime," +
+            "first_employment_unit as firstEmploymentUnit,tutor_number as tutorNumber,assistant_tutor_number as assistantTutorNumber," +
+            "birthday,email,mobile_phone as mobilePhone,remarks,photo" +
+            "from t_student WHERE `graduation_time` > now()")
+    List<Student> selectGraduate();
+
+    //对t_static_content表格进行操作
+    @Select("SELECT * FROM t_static_content WHERE `t_static_content`.id = #{param1}")
+    StaticContent selectStaticContent(int staticContentId);
+
+    //对t_dynamic表格操作
+    @Select("SELECT `id`,`title` ,`time`,`editor_name` as editorName ,`content`,`category`" +
+            "FROM t_dynamic limit offset,pageSize" +
+            "WHERE `category` = #{param1}")
+    List<Dynamic> selectDynamicByType(int type,int offset,int pageSize);
+    @Select("SELECT `id`,`title` ,`time`,`editor_name` as editorName ,`content`,`category`" +
+            "FROM t_dynamic" +
+            "WHERE t_dynamic.`id` = #{param1}")
+    Dynamic selectDynamic(int dynamicId);
+    @Select("SELECT `id`,`title` ,`time`,`editor_name` as editorName ,`content`,`category`" +
+            "FROM t_dynamic WHERE" +
+            "WHERE (`title` like %#{param2}% or `content` like %#{param2}%) AND `category' = #{param1}"
+    )
+    List<Dynamic> selectDynamicByCondition(int type,String condition);
+
+    //对t_teaching_information表格操作
+    @Select("SELECT `id`,`numbering`,`name`,`properties`,`teacher_number` as teacherNumber,`teaching_time`,`student_level` as student_Level" +
+            "FROM t_teaching_information LIMIT offset,pageSize")
+    List<TeachingInformation> selectTeachingInformation(int offset,int pageSize);
+    @Select("SELECT `id`,`numbering`,`name`,`properties`,`teacher_number` as teacherNumber,`teaching_time`,`student_level` as student_Level" +
+            "FROM t_teaching_information" +
+            "WHERE `name` like %#{param1}% or `teaching_time` like %#{param1}% or `student_level` like %#{param1}%"
+    )
+    List<TeachingInformation> selectTeachingInformationByCondition(String condition);
+
+    //对t_scientific_information表格操作
+    @Select("SELECT `id`,`properties`,`name`,`teacherNumber`" +
+            "FROM t_scientific_information LIMIT offset,pageSize")
+    List<ScientificInformation> selectScientificInformation(int offset,int pageSize);
+    @Select("SELECT `id`,`properties`,`name`,`teacherNumber`" +
+            "FROM t_scientific_information" +
+            "WHERE `name` like %#{param1}% or `teacher_number` like %#{param1}%")
+    List<ScientificInformation> selectScientificInformationByCondition(String condition);
+
+    //对t_research_result表格操作
+    @Select("SELECT * FROM t_research_result LIMIT offset,pageSize")
+    List<ResearchResult> selectResearchResult(int offset,int pageSize);
+    @Select("SELECT * FROM t_research_result WHERE `name` like %#{param1}%")
+    List<ResearchResult> selectResearchResultByCondition(String condition);
+
+    //对t_teach_result表格操作
+    @Select("SELECT * FROM t_teach_result LIMIT offset,pageSize")
+    List<TeachResult> selectTeachResult(int offset,int pageSize);
+    @Select("SELECT * FROM t_teach_result WHERE `name` like %#{param1}% ")
+    List<TeachResult> selectTeachResultByCondition(String condition);
+
+    //对t_research_paper表格操作
+    @Select("SELECT `id`,`title`,`conference`,`conference_id` as conferenceId,`year`" +
+            "FROM t_research_paper LIMIT offset,pageSize")
+    List<ResearchPaper> selectResearchPaper(int offset,int pageSize);
+    @Select("SELECT `id`,`title`,`conference`,`conference_id` as conferenceId,`year`" +
+            "FROM t_research_paper" +
+            "WHERE `title` like %#{param1}% or `conference` like %#{param1}%")
+    List<ResearchPaper> selectResearchPaperByCondition(String condition);
+
+    //对t_teach_paper表格操作
+    @Select("SELECT `id`,`title`,`conference`,`conference_id` as conferenceId,`year`" +
+            "FROM t_teach_paper LIMIT offset,pageSize")
+    List<TeachPaper> selectTeachPaper(int offset,int pageSize);
+    @Select("SELECT `id`,`title`,`conference`,`conference_id` as conferenceId,`year`" +
+            "FROM t_teach_paper" +
+            "WHERE `title` like %#{param1}% or `conference` like %#{param1}%")
+    List<TeachPaper> selectTeachPaperByCondition(String condition);
 
 
 }
