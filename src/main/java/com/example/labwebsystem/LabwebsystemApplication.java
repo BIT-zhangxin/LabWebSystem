78585446f8344ee44b13dda6dda127bbd9cb7abe
[BIT-zhangxin] <1047632321@qq.com>
2019-09-15 21:44:33 +0800

    添加打包代码

--- src/main/java/com/example/labwebsystem/LabwebsystemApplication.java
+++ src/main/java/com/example/labwebsystem/LabwebsystemApplication.java
@@ -3,6 +3,7 @@ package com.example.labwebsystem;
 import com.example.labwebsystem.security.service.AppUserDetailArgumentResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,7 +14,7 @@ import java.util.List;
 @EnableSwagger2
 @EnableTransactionManagement
 @SpringBootApplication
-public class LabwebsystemApplication implements WebMvcConfigurer {
+public class LabwebsystemApplication extends SpringBootServletInitializer implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(LabwebsystemApplication.class, args);
@@ -23,5 +24,11 @@ public class LabwebsystemApplication implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new AppUserDetailArgumentResolver());
     }
+
+//    @Override//为了打包springboot项目
+//    protected SpringApplicationBuilder configure(
+//            SpringApplicationBuilder builder) {
+//        return builder.sources(this.getClass());
+//    }
 
 }
